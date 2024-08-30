e2d4574ff12b1a2f1ffe55cac3affd2ffbdc54ad
andrei <andreimendes8@gmail.com>
2024-08-30 17:49:47 -0300

    novos cadastros

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,54 @@
 import './style.css'
+import Trash from '../../assets/trash.svg'
 
 function Home() {
+
+  const users = [
+    {
+      id: '5565656646552646sf',
+      name: 'Andrei',
+      age: 27,
+      email: 'andreimendes8@gmail.com'
+    },
+    {
+      id: '55656454556552646sf',
+      name: 'Lucas',
+      age: 29,
+      email: 'lucaslegal@gmail.com'
+    },
+    {
+      id: '556766454556552646sf',
+      name: 'Cleitin',
+      age: 80,
+      email: 'cleitorasta@gmail.com'
+    }
+  ]
+
   return (
-    <div>
-      <h1 className='text-white'>Olá React</h1>
+    <div className='container'>
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input placeholder='Name' name='name' type='text' />
+        <input placeholder='Age' name='age' type='number' />
+        <input placeholder='E-mail' name='email' type='email' />
+        <button type='button'>Cadastrar</button>
+      </form>
+
+      {users.map(user => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Name: <span>{user.name}</span></p>
+            <p>Age: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <button>
+            <img src={Trash} alt="" />
+          </button>
+        </div>
+
+      ))}
+
+
     </div>
   )
 }
