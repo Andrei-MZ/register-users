38363f01166d6ab4e5821924c1ae1fb55c391055
andrei <andreimendes8@gmail.com>
2024-09-02 10:14:48 -0300

    funcções cadastrar e excluir

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,45 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './style.css'
 import Trash from '../../assets/trash.svg'
 import api from '../../services/api'
 
+/**
+ * React Hooks
+ * - useEffect 
+ * - useState
+ * - useRef
+ */
+
 function Home() {
   const [users, setUsers] = useState([])
 
-  async function getUsers(){
-    const userFromApi = await api.get('/users')
-    /**
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  /**
      * Quando eu quiser alterar o valor do 'user' eu preciso pegar o 'setUsers' e setUsers (userFromApi.data) a informação que quero passar
      */
+  async function getUsers(){
+    const userFromApi = await api.get('/users')
+    
     setUsers (userFromApi.data)
-    console.log(users)
+  }
+
+  async function createUsers(){
+    await api.post('/users', {
+    name: inputName.current.value,
+    age: inputAge.current.value,
+    email: inputEmail.current.value
+  })
+
+    getUsers()
+  }
+
+  async function deleteUsers(id){ 
+    await api.delete(`/users/${id}`)
+  
+    getUsers()
   }
 
   useEffect(() => {
@@ -25,10 +52,10 @@ function Home() {
     <div className='container'>
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input placeholder='Name' name='name' type='text' />
-        <input placeholder='Age' name='age' type='number' />
-        <input placeholder='E-mail' name='email' type='email' />
-        <button type='button'>Cadastrar</button>
+        <input placeholder='Name' name='name' type='text' ref={inputName}/>
+        <input placeholder='Age' name='age' type='number' ref={inputAge}/>
+        <input placeholder='E-mail' name='email' type='email' ref={inputEmail}/>
+        <button type='button' onClick={createUsers}>Cadastrar</button>
       </form>
 
       {users.map(user => (
@@ -38,7 +65,7 @@ function Home() {
             <p>Age: <span>{user.age}</span></p>
             <p>Email: <span>{user.email}</span></p>
           </div>
-          <button>
+          <button onClick={() => deleteUsers(user.id)}>
             <img src={Trash} alt="" />
           </button>
         </div>
